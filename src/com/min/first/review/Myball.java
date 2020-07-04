2bc584d4d6030a9f2cd50c293a24b5a030fcb286
pcm93 <pcm93@192.168.1.167>
2020-07-04 13:39:28 +0900

    야구게임 객체지향 연습

---
+++ src/com/min/first/review/Myball.java
@@ -0,0 +1,26 @@
+package com.min.first.review;
+
+import java.util.Scanner;
+
+public class Myball {
+
+	private int[] myArr;
+	private Scanner sc;
+	
+	
+	public void Myball(int num) {
+		myArr = new int[num];
+		sc = new Scanner(System.in);
+	}
+	
+	public void setNumbers() {
+		
+		for(int i =0; i<myArr.length; i++) {
+			System.out.printf("숫자 %d :",i+1);
+			int val = sc.nextInt();
+		}
+	}
+	
+	
+	
+}
