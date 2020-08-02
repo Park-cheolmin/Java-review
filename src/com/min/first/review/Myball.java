2537ecb9cecdd65df8a887f0abacd4af39b6b510
park-cheolmin <pcm9307@naver.com>
2020-08-02 23:57:21 +0900

    20.08.02 야구게임(myball)

--- src/com/min/first/review/Myball.java
+++ src/com/min/first/review/Myball.java
@@ -7,20 +7,52 @@ public class Myball {
 	private int[] myArr;
 	private Scanner sc;
 	
+	public void MyBall(int num) {
+		init(num);
+	}
 	
-	public void Myball(int num) {
+	private void init(int num) {
 		myArr = new int[num];
 		sc = new Scanner(System.in);
 	}
 	
-	public void setNumbers() {
+	public void setNumbers() {  //사용자 입력 숫자 n개
 		
-		for(int i =0; i<myArr.length; i++) {
+		for(int i =0; i<myArr.length;i++) {
 			System.out.printf("숫자 %d :",i+1);
-			int val = sc.nextInt();
+			String val = sc.nextLine();
+			
+			try {
+				myArr[i] = Integer.parseInt(val);
+			} catch(Exception e) {
+				i--;
+				System.out.println("숫자만 입력하세요.");
+				continue;
+			}
+					
+			if(myArr[i]<1 || myArr[i]>9) {
+				System.out.println("1~9 범위 이내의 숫자를 입력해주세요");
+				i--;
+				continue;
+			}
+			
+			
+			for(int z=0; z<i; z++) {
+				if(myArr[i]==myArr[z]){
+					System.out.println("중복된 숫자를 입력하셨습니다");
+					i--;
+					break;
+				}
+			
+			}
+		
 		}
+		
 	}
 	
+	public int get(int idx) {
+		return myArr[idx];
+	}
 	
 	
 }
